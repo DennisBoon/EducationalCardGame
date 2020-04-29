b76ef06cf6fba47d50394b02ad364fcb5b72550d
Dennis Boon <dennis.boon96@gmail.com>
2020-04-29 13:43:23 +0200

    Added networkmanager to testing project

---
+++ EducationalCardGame_TESTING/Assets/_Scripts/NetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviour, IOnEventCallback
+{
+    public const byte InstantiateVrAvatarEventCode = 1; // example code, change to any value between 1 and 199
+
+    void IOnEventCallback.OnEvent(EventData photonEvent)
+    {
+        if (photonEvent.Code == InstantiateVrAvatarEventCode)
+        {
+            GameObject remoteAvatar = Instantiate(Resources.Load("RemoteAvatar")) as GameObject;
+            PhotonView photonView = remoteAvatar.GetComponent<PhotonView>();
+            photonView.ViewID = (int)photonEvent.CustomData;
+        }
+    }
+}
