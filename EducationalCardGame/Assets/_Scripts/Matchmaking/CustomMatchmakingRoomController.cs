63ed296cea2112db8aac962d1e96ab1d956b834e
Dennis Boon <dennis.boon96@gmail.com>
2020-05-21 14:08:15 +0200

    Added CustomMatchmaking system

---
+++ EducationalCardGame/Assets/_Scripts/Matchmaking/CustomMatchmakingRoomController.cs
@@ -0,0 +1,105 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CustomMatchmakingRoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int multiPlayerSceneIndex;
+
+    [SerializeField]
+    private GameObject lobbyPanel;
+    [SerializeField]
+    private GameObject roomPanel;
+
+    [SerializeField]
+    private GameObject startButton;
+
+    [SerializeField]
+    private Transform playersContainer;
+    [SerializeField]
+    private GameObject playerListingPrefab;
+
+    [SerializeField]
+    private TMP_Text roomNameDisplay;
+
+    void ClearPlayerListings()
+    {
+        for (int i = playersContainer.childCount - 1; i >= 0; i--)
+        {
+            Destroy(playersContainer.GetChild(i).gameObject);
+        }
+    }
+
+    void ListPlayers()
+    {
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            GameObject tempListing = Instantiate(playerListingPrefab, playersContainer);
+            TMP_Text tempText = tempListing.transform.GetChild(0).GetComponent<TMP_Text>();
+            tempText.text = player.NickName;
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        roomPanel.SetActive(true);
+        lobbyPanel.SetActive(false);
+        roomNameDisplay.text = PhotonNetwork.CurrentRoom.Name;
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            startButton.SetActive(true);
+        }
+        else
+        {
+            startButton.SetActive(false);
+        }
+        //photonPlayers = PhotonNetwork.PlayerList;
+        ClearPlayerListings();
+        ListPlayers();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        ClearPlayerListings();
+        ListPlayers();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        ClearPlayerListings();
+        ListPlayers();
+        if (PhotonNetwork.IsMasterClient)
+        {
+            startButton.SetActive(true);
+        }
+    }
+
+    public void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            PhotonNetwork.LoadLevel(multiPlayerSceneIndex);
+        }
+    }
+
+    IEnumerator rejoinLobby()
+    {
+        yield return new WaitForSeconds(1);
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void BackOnClick()
+    {
+        lobbyPanel.SetActive(true);
+        roomPanel.SetActive(false);
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LeaveLobby();
+        StartCoroutine(rejoinLobby());
+    }
+}
